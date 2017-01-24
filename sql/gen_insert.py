c15c1d316ece0de5f9011689ac13c52e14cc08ae
Joseph <j.d.livni@gmail.com>
2017-01-24 06:55:49 +0000

    insertion working now!

--- sql/gen_insert.py
+++ sql/gen_insert.py
@@ -3,7 +3,7 @@ import csv
 import psycopg2
 import random
 
-db_name = "test_user"
+db_name = "lost"
 port = 5432
 cursor = None
 conn = None
@@ -17,8 +17,9 @@ def main():
         try:
             lines = csv.reader(file, delimiter=',')
             if "inventory" in file.name:
-                inventory(lines)
-                print("*****************************************")
+                name = file.name[len(fileDirectory) - len(file.name) + 1: -4]
+                facility(name)
+                inventory(lines, name)
             elif "product_list" in file.name:
                 product_list(lines)
             elif "transit" in file.name:
@@ -26,33 +27,49 @@ def main():
         except Exception as e:
             print("Error: " + str(e))
 
+def facility(name):
+    insert("facilities", ["fcode"], [name])
+
+
 def transit(lines):
+    global cursor
     header = next(lines)
     for row in lines:
-        print(cursor.execute("SELECT asset_tag FROM assets WHERE asset_pk=3;"))
-        if(cursor.execute("SELECT count(1) FROM facilities WHERE common_name="+"'"+row[1]+"'"+";") == 0):
-            print("testing")
-            insert("facilities", ["common_name", "depart_dt"], [row[1], row[3]])
+        cursor.execute("SELECT facility_pk FROM facilities WHERE common_name="+"'"+row[1]+"'"+";")
+        if cursor.fetchone() == None:
+            insert("facilities", ["common_name"], [row[1]])
         
-        if(cursor.execute("SELECT count(1) FROM facilities WHERE common_name="+"'"+row[2]+"'"+";") == 0):
-            insert("facilities", ["common_name", "arrive_at"], [row[2], row[4]])
-            print("testign2")
-        if(cursor.execute("SELECT count(1) FROM assets WHERE asset_tag="+"'"+row[0]+"'"+";") == 0):
+        cursor.execute("SELECT facility_pk FROM facilities WHERE common_name="+"'"+row[2]+"'"+";")
+        if cursor.fetchone() == None:
+            insert("facilities", ["common_name"], [row[2]])
+        
+        cursor.execute("SELECT  FROM assets WHERE asset_tag="+"'"+row[0]+"'"+";")
+        if cursor.fetchone() == None:
             insert("assets", ["asset_tag", "description"], [row[0], row[6]])
-            print("testing3")
-        asset_fk = cursor.execute("SELECT asset_pk FROM assets WHERE asset_tag="+"'"+row[0]+"'"+";")
-        facility_fk1 = cursor.execute("SELECT facility_pk FROM facilities WHERE common_name="+"'"+row[1]+"'" +";")
-        facility_fk2 = cursor.execute("SELECT facility_pk FROM facilities WHERE common_name="+"'"+row[2]+"'"+";")
-        
-        print(asset_fk)
-        insert("asset_at", ["asset_fk", "facility_fk", "depart_dt"], [asset_fk, facility_fk2, row[4]])
-        insert("asset_at", ["asset_fk", "facility_fk", "arrive_dt"], [asset_fk, facility_fk1, row[3]]) 
 
-def inventory(lines):
+        cursor.execute("SELECT asset_pk FROM assets WHERE asset_tag="+"'"+row[0]+"'"+";")
+        asset_fk = cursor.fetchone()
+        cursor.execute("SELECT facility_pk FROM facilities WHERE common_name="+"'"+row[1]+"'" +";")
+        facility_fk1 = cursor.fetchone()
+        cursor.execute("SELECT facility_pk FROM facilities WHERE common_name="+"'"+row[2]+"'"+";")
+        facility_fk2 = cursor.fetchone()
+
+        insert("asset_at", ["asset_fk", "facility_fk", "depart_dt"], [asset_fk[0], facility_fk2[0], row[4]])
+        insert("asset_at", ["asset_fk", "facility_fk", "arrive_dt"], [asset_fk[0], facility_fk1[0], row[3]]) 
+
+def inventory(lines, name):
+    global cursor
     header = next(lines)
     for row in lines:
         insert("products", ["description"], [row[1]])
         insert("assets", ["asset_tag"], [row[0]])
+        
+        cursor.execute("SELECT asset_pk FROM assets WHERE asset_tag="+"'"+row[0]+"'"+ ";")
+        asset_fk = cursor.fetchone()
+        cursor.execute("SELECT facility_pk FROM facilities WHERE fcode="+"'"+name+"'"+ ";")
+        facility_fk = cursor.fetchone()
+        insert("asset_at", ["asset_fk", "facility_fk"], [asset_fk[0], facility_fk[0]])
+
 
 def insert(table, keys, values):
     global cursor
@@ -68,7 +85,7 @@ def insert(table, keys, values):
         if i < len(values) - 1:
             valueResult += ","
     
-    print("INSERT INTO {} ({}) VALUES ({});".format(table, ",".join(keys), valueResult))
+   # print("INSERT INTO {} ({}) VALUES ({});".format(table, ",".join(keys), valueResult))
     cursor.execute("INSERT INTO {} ({}) VALUES ({});".format(table, ",".join(keys), valueResult))
     conn.commit()
 
