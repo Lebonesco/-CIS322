4e9e51693d3d5cb3aa0dfef0b3e9847eaa82c426
Joseph <j.d.livni@gmail.com>
2017-03-07 07:48:55 +0000

    header styling

--- export/exportCSV.py
+++ export/exportCSV.py
@@ -7,7 +7,6 @@ def main():
     DB_NAME = sys.argv[2]
     PORT = sys.argv[3]
     HOST = sys.argv[4]
-    #print("Argument list: ", str(sys.argv))
     
     conn = psycopg2.connect(dbname=DB_NAME, host=HOST, port=PORT)
     cur = conn.cursor()
@@ -15,21 +14,17 @@ def main():
     rows = cur.fetchall()
     cur.execute("SELECT column_name FROM information_schema.columns WHERE table_name='"+TABLE_NAME+"'")
     headers = cur.fetchall()
-    print("headers: ", headers)
-    print("rows: ", rows)
+
+    headers = list(map(lambda x: x[0], headers))
+
     with open(TABLE_NAME+".csv", 'w') as file:
+
         wr = csv.writer(file)
         wr.writerow(headers)
         for i in range(len(rows)):
             wr.writerow(rows[i])
-
-
+    
     conn.close()
-
-
-
-
-
 
 if __name__ == '__main__':
     main()
